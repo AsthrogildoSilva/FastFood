68c63eb6a8b3fe28f11e6973db4e36b3016542df
AsthrogildoSilva <asthrogildo.silva@outlook.com>
2022-07-16 23:10:02 -0300

    Criando uma Calculadora Simples.

---
+++ BootCampFullStackSantaderDio/InteliJCodes/Java/src/ExercicioMetodosDio/CalculadoraSimples.java
@@ -0,0 +1,21 @@
+package ExercicioMetodosDio;
+
+public class CalculadoraSimples {
+
+    public static void soma(double numero1, double numero2) {
+        double resultado = numero1 + numero2;
+        System.out.println("O resultado da soma é: " + resultado);
+    }
+    public static void subtracao(double numero1, double numero2) {
+        double resultado = numero1 - numero2;
+        System.out.println("O resultado da Subtração é: " + resultado);
+    }
+    public static void multiplicacao(double numero1, double numero2) {
+        double resultado = numero1 * numero2;
+        System.out.println("O resultado da multiplicação é: " + resultado);
+    }
+    public static void divisao(double numero1, double numero2) {
+        double resultado = numero1 / numero2;
+        System.out.println("O resultado da dívisão é: " + resultado);
+    }
+}
---
+++ BootCampFullStackSantaderDio/InteliJCodes/Java/src/ExercicioMetodosDio/main.java
@@ -0,0 +1,9 @@
+package ExercicioMetodosDio;
+
+public class main {
+    public static void main(String[] args) {
+
+        CalculadoraSimples.soma(5, 10);
+
+    }
+}
